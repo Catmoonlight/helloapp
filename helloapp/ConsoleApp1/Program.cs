abaafac696372e0a5ae288aa580c290a037ac70a
black <black@example.com>
2021-11-03 22:20:07 +0500

    =Call methods

--- helloapp/ConsoleApp1/Program.cs
+++ helloapp/ConsoleApp1/Program.cs
@@ -17,6 +17,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Everyone!");
+            GreetBlack();
+            GreetWhite();
         }
     }
 }
